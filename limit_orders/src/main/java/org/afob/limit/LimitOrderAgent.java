44b6bd8bfb58e5905d7b7aa81b34adc8faf25fb3
LovelyUpadhyay01 <lovely.upadhyay@in.ey.com>
2024-08-22 23:45:21 +0530

    Update LimitOrderAgent.java

--- limit_orders/src/main/java/org/afob/limit/LimitOrderAgent.java
+++ limit_orders/src/main/java/org/afob/limit/LimitOrderAgent.java
@@ -7,13 +7,41 @@ import java.math.BigDecimal;
 
 public class LimitOrderAgent implements PriceListener {
 
+    private final ExecutionClient m_executionClient ;
     public LimitOrderAgent(final ExecutionClient ec) {
+        m_executionClient = ec;
     }
 
 
     @Override
     public void priceTick(String productId, BigDecimal price) {
-
+        
+        if ( "IBM".equals(productId) && price != null && price < 100) {
+            m_executionClient.buy();
+        }    
     }
+
+    public void addOrder(TransactionType transactionType, String productId, 
+                        int  amount, int limit) {
+
+        if( transactionType == TransactionType.BUY) {
+            if ( "IBM".equals(productId)  &&  price < 100) {
+               
+                m_executionClient.buy(productId, amount);
+            } else {
+                m_executionClient.buy(productId, amount);
+            }
+        } else if (transactionType == TransactionType.SELL) {
+
+            m_executionClient.sell(productId, amount);
+        }
+        
+    }
+
+}
+
+public enum TransactionType {
+    BUY,
+    SELL;
 
 }

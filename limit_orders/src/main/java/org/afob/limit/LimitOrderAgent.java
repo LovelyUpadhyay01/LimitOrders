b303af99b848da31fcb3fea368b49d26419a5f3e
LovelyUpadhyay01 <lovely.upadhyay@in.ey.com>
2024-08-23 09:39:50 +0530

    updating code

--- limit_orders/src/main/java/org/afob/limit/LimitOrderAgent.java
+++ limit_orders/src/main/java/org/afob/limit/LimitOrderAgent.java
@@ -12,33 +12,73 @@ public class LimitOrderAgent implements PriceListener {
         m_executionClient = ec;
     }
 
+    List<Order> cacheOrders = new ArrayList<>();
 
     @Override
     public void priceTick(String productId, BigDecimal price) {
         
-        if ( "IBM".equals(productId) && price != null && price < 100) {
-            m_executionClient.buy();
-        }    
+        if( transactionType == TransactionType.BUY) {
+            if ( price <= limit) {
+               
+                m_executionClient.buy(productId, amount);
+            } 
+        } else if (transactionType == TransactionType.SELL) {
+            
+            m_executionClient.sell(productId, amount);
+        }   
     }
 
     public void addOrder(TransactionType transactionType, String productId, 
                         int  amount, int limit) {
 
-        if( transactionType == TransactionType.BUY) {
-            if ( "IBM".equals(productId)  &&  price < 100) {
-               
-                m_executionClient.buy(productId, amount);
-            } else {
-                m_executionClient.buy(productId, amount);
-            }
-        } else if (transactionType == TransactionType.SELL) {
-
-            m_executionClient.sell(productId, amount);
-        }
+        cacheOrders.add(new Order(transactionType, productId, amount, limit));
         
     }
 
+    private static class Order {
+        TransactionType transactionType;
+        String productId;
+        int amount;
+        int limit;
+
+        Order(TransactionType transactionType, String productId, 
+                        int  amount, int limit) {
+             this.transactionType = transactionType;
+             this.productId = productId;
+             this.amount = amount;
+             this.limit = limit;               
+        }
+
+        public void setTransactionType(TransactionType ty){
+            this.transactionType = ty;
+        }
+        public void setProductId(int id){
+            this.productId = id;
+        }
+        public void setAmount(int amount){
+            this.amount = amount;
+        }
+        public void setLimit(int limit){
+            this.limit = limit;
+        }
+
+        public TransactionType getTransactionType(){
+            return transactionType;
+        }
+
+        public String getProductId(){
+            return productId;
+        }
+
+        public int getAmount(){
+            return amount;
+        }
+        public int getLimit(){
+            return limit;
+        }
+    }
 }
+
 
 public enum TransactionType {
     BUY,
